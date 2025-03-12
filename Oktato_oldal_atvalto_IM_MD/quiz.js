314be0748f4b9dcc4a5f48b2e6f19df14ca3c00a
Pillowpilot1 <ivanyimatyi333@gmail.com>
2025-03-12 09:23:27 +0100

    03.12

---
+++ Oktato_oldal_atvalto_IM_MD/quiz.js
@@ -0,0 +1,17 @@
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+const kerdesek = [
+    {
+        question: "A kettes számrendszer helyiértékes számrendszer:",
+        answers: {
+          a: "jobbról balra haladva minden egyes számjegy a 2 eggyel nagyobb",
+          b: "jobbról balra haladva minden egyes számjegy a 2 eggyel kisebb",
+          c: "balról jobbra haladva minden egyes számjegy a 2 eggyel kisebb"
+        },
+        correctAnswer: "a"
+      },
+
+
+]
